179fc2351fa8838a3f21fdd82fd8c5b1db1ce4b5
sachin <sangwansachin631@gmail.com>
2024-01-10 15:37:47 +0530

    populating

--- controolers/classController.js
+++ controolers/classController.js
@@ -1,4 +1,5 @@
 const classModel = require("../models/classModel");
+const userModel = require("../models/registerUser");
 
 const classController={
     storeClass:async(req,res)=>{
@@ -9,6 +10,14 @@ const classController={
         });
         await storeData.save();
         res.status(200).json({success:true,message:"class store successFully",class:storeData})
+    },
+    updateClass:async(req,res)=>{
+        const findUser=await userModel.findOne({_id:req.body._id}).populate("class_id")
+        newId=req.body.newId;
+        findUser.class_id=newId;
+        await findUser.save();
+        console.log(findUser,"findUser");
+        res.status(200).json({user:findUser}); 
     }
 }
 
--- routers/classRouter.js
+++ routers/classRouter.js
@@ -1,7 +1,8 @@
 const express=require("express");
-const { storeClass } = require("../controolers/classController");
+const { storeClass, updateClass } = require("../controolers/classController");
 const classRouter=express.Router();
 
 classRouter.post("/storeClass",storeClass);
+classRouter.post("/updateClass",updateClass)
 
 module.exports=classRouter;
